b5b213fd32a336c5e9f5f3aca96c7176979cf98d
Maria <mariadelrociocabello@gmail.com>
2020-02-05 16:59:58 +0100

    Modified T12mni_ants_wf script

--- T12mni_ants_wf.py
+++ T12mni_ants_wf.py
@@ -29,11 +29,35 @@ def get_ants_normalize_T1_MNI ():
         'MNI_ref_img',
     ]),
     name='input_node')    
+    
+    # ¿Se dan los inputs así?
+    node_T12mni = Node(ants.Registration(
+        transformations=['Rigid', 'Affine', 'Syn'],
+        input_metric=['MI']*3,
+        metric_weight=[1]*3,
+        shrink_factors=[[8,4,2,1],[8,4,2,1],[8,4,2,1]],
+        smoothing_sigmas=[[3,2,1,0], [3,2,1,0], [3,2,1,0]],
+        radius_or_number_of_bins=[32]*3,
+        transform_parameters=[[0.1],[0.1],[0.1,3.0]],
+        number_of_iterations=[[1000,500,250,100],[1000,500,250,100],[1000,500,250,100]],
+        write_composite_transform=True,
+    ),
+    name='T12mni_node')
 
+    # Se recomienda usar apply
     node_output = Node(utility.IdentityInterface(fields=[
         'struct2MNI_warp', 
         'struct2MNI_img'
     ]),
     name='output_node') 
+    
+    wf.connect([
+#inputs         
+                (node_input, node_T12mni, [("T1_img", "moving_image")]),
+                (node_input, node_T12mni, [("MNI_ref_img", "fixed_image")]),
+#yeld relevant data to output node
+                (node_T12mni , node_output, [("composite_transform", "struct2MNI_warp")]),
+                (node_T12mni, node_output,[("warped_image","struct2MNI_img")]),               
+    ])
                                  
     return(wf)
