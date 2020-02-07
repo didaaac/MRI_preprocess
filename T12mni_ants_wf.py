9113c7b937269b5f5a67aaa8ef26225cc7b65368
Maria <mariadelrociocabello@gmail.com>
2020-02-07 10:33:12 +0100

    Modified T12mni_ants_wf + tester (not working)

--- T12mni_ants_wf.py
+++ T12mni_ants_wf.py
@@ -20,27 +20,27 @@ def get_ants_normalize_T1_MNI ():
     from nipype.interfaces import ants,utility 
     
      #Defines workflow
-    wf=Workflow(name='Normalize_Struct2MNI');
+    wf=Workflow(name='Normalize_Struct2MNI', base_dir='');
     
     
     #Setting INPUT node...
     node_input = Node(utility.IdentityInterface(fields=[
-        'T1_img', 
+        'T1_img',
         'MNI_ref_img',
     ]),
     name='input_node')    
     
     # ¿Se dan los inputs así?
     node_T12mni = Node(ants.Registration(
-        transformations=['Rigid', 'Affine', 'Syn'],
-        input_metric=['MI']*3,
-        metric_weight=[1]*3,
+        transforms=['Rigid', 'Affine', 'SyN'],
         shrink_factors=[[8,4,2,1],[8,4,2,1],[8,4,2,1]],
         smoothing_sigmas=[[3,2,1,0], [3,2,1,0], [3,2,1,0]],
         radius_or_number_of_bins=[32]*3,
-        transform_parameters=[[0.1],[0.1],[0.1,3.0]],
+        metric = ['MI']*3,
+        transform_parameters=[(0.1,),(0.1,),(0.1,3,0)],
         number_of_iterations=[[1000,500,250,100],[1000,500,250,100],[1000,500,250,100]],
         write_composite_transform=True,
+        metric_weight=[1]*3,
     ),
     name='T12mni_node')
 
@@ -61,3 +61,4 @@ def get_ants_normalize_T1_MNI ():
     ])
                                  
     return(wf)
+
