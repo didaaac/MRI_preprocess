568a0cffb78f1a46c6f454022b03cd58981364a6
Maria <mariadelrociocabello@gmail.com>
2020-03-06 10:24:22 +0100

    Added bash for high-resolution (0.8mm) recon-all

---
+++ Structural/highres_08mm_reconall_T1T2.sh
@@ -0,0 +1,91 @@
+#BATCH for freesurfer -- bash
+#Enter subject_dir for freesurfer output folder
+#Enter BIDs input folder
+#Processes multiple T1 runs
+#Careful. Doesn't take into account T2 other than the first run... User should ckeck its quality
+#No high-res processing so far though it could be done and may be preferable
+#Dídac Macià Bros, 25-02-2019
+
+#**************** USAGE ******************
+#highres_08mm_reconall_T1T2.sh -o SUBJECTS_DIR_FREESURFER -i BIDS_DIRECTORY -p num_parallel_cores -e expert_file -c choosen_subjects_dir
+#/<where_this_script_is>/highres_08mm_reconall_T1T2.sh -o /home/mariacabello/wf_workspace/CHROID_PLEXUS/execution_5subjects_08mm -i /institut/processed_data/BBHI_structural -p 4 -e /home/mariacabello/git_projects/MRI_preprocess/Structural/expert_08mm.opts -c /home/mariacabello/wf_workspace/CHROID_PLEXUS/execution_5subjects_1mm
+#*****************************************
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -o|--output_dir)
+    SUBJECTS_DIR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -i|--input_dir)
+    BIDS_FOLDER="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -p|--pcores)
+    PCORES="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -e|--input_dir)
+    EXPERT_FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -c|--choosen_subjects_dir)
+    CHOOSEN_SUBJECTS_DIR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+
+
+#DEFAULT VALUES
+PCORES=4
+
+#gets params from terminal call
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo SUBJECT_DIR     = "${SUBJECTS_DIR}"
+echo BIDS_DIR        = "${BIDS_FOLDER}"
+
+echo PARALLEL CORES  = "${PCORES}"
+
+
+#export SUBJECTS_DIR='/institut/processed_data/BBHI_output/structural'
+#BIDS_FOLDER='/institut/processed_data/BBHI_structural'
+
+for d in $CHOOSEN_SUBJECTS_DIR/*/ ; do
+    run_counter=0
+    for nrun in $BIDS_FOLDER/$(basename $d)/ses-01/anat/$(basename $d)_ses-01_run-*_T1w.nii.gz ; do  
+      run_counter=$((run_counter+1))
+      
+      if ((run_counter>1)) ; then
+ SUBJECT_ID=$(basename $d)_run_$run_counter
+ echo "REPEATED RUN num_$run_counter"
+      else
+ SUBJECT_ID=$(basename $d)
+      fi
+     
+      if [ -e $SUBJECTS_DIR/$SUBJECT_ID ] ;then
+	echo "$SUBJECT_ID is already processed. Skipping..."
+      else
+	echo "Processing $SUBJECT_ID ... with source image : $nrun"
+	recon-all -all -s $SUBJECT_ID -hires -i $nrun -T2 $BIDS_FOLDER/$(basename $d)/ses-01/anat/$(basename $d)_ses-01_run-01_T2w.nii.gz -T2pial -openmp $PCORES -3T  -expert $EXPERT_FILE 
+      fi
+    done 
+done
+
+#-highres -expert $EXPERT_HIGHRES_FILE \ 
+
+
